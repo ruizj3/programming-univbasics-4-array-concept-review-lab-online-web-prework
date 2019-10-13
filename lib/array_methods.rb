3bb8fed62ba276dd3d120291ad29632480f8c317
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 23:10:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max_by do |element|
+  array.max_by { |element|
   element.field
+ } 
 end
 
 def find_min_value(array)
