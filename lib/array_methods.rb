34f3ab11b367efa70e6de7cb718e0890eddbb816
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 23:12:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-    array.index { |value_to_find|
-      array.include?(value_to_find)
+    array.find_index { |item|
+      item = value_to_find
     }
 end
 
